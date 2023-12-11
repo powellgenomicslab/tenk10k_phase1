8e930514bebb59761c9b8937d97204d7d1b9084b
Anna Cuomo <annacuomo92@gmail.com>
2023-12-11 11:49:00 +1100

    Create scvi_integrate.py

---
+++ integration/scvi_integrate.py
@@ -0,0 +1,47 @@
+import glob
+import time
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import scvi
+
+# CellRanger files
+cellranger_dir = "/directflow/SCCGGroupShare/projects/data/experimental_data/projects/TenK10K/GencodeV44/"
+files = glob.glob(cellranger_dir+"S*")
+
+numbers_to_combine = [2,5,10,20,40,64]
+data = {"nsamples": numbers_to_combine, "ncells": 0, "ngenes": 0, "concat_time": 0, "pca_time": 0, "scvi_time": 0}
+summary_df = pd.DataFrame(data)
+summary_filename = "/share/ScratchGeneral/anncuo/tenk10k/data_processing/integration/scvi_etal_times_summary.csv"
+
+for num in numbers_to_combine:
+    print(num)
+    datasets = []
+    for file in files[0:num]:
+        sample = file.replace(cellranger_dir, "")
+        adata=sc.read_10x_h5(file+"/outs/filtered_feature_bc_matrix.h5")
+        adata.var_names_make_unique()
+        adata.obs['sample'] = sample
+        adata.obs['barcode'] = adata.obs.index
+        adata.obs.index = sample + "_" + adata.obs['barcode']
+        datasets.append(adata)
+    rowIndex = summary_df[summary_df['nsamples']==num].index[0]
+    start_time = time.time()
+    adata_all = datasets[0].concatenate(*datasets[1:])
+    summary_df.at[rowIndex,'concat_time'] = time.time() - start_time
+    summary_df.at[rowIndex,'ncells'] = adata_all.shape[0]
+    summary_df.at[rowIndex,'ngenes'] = adata_all.shape[1]
+    start_time = time.time()
+    sc.tl.pca(adata_all, svd_solver='arpack')
+    summary_df.at[rowIndex,'pca_time'] = time.time() - start_time
+    scvi.model.SCVI.setup_anndata(adata_all, layer = "counts",
+                             categorical_covariate_keys=["sample"],
+                             continuous_covariate_keys=['pct_counts_mt', 'total_counts'])
+    model = scvi.model.SCVI(adata)
+    start_time = time.time()
+    model.train()
+    summary_df.at[rowIndex,'scvi_time'] = time.time() - start_time
+    summary_df.to_csv(summary_filename)
+
+
+print(summary_df.head())
