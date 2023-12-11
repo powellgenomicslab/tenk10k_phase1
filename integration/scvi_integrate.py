64486816b738d6abae148b01dea8deae9be2cc6f
Anna Cuomo <annacuomo92@gmail.com>
2023-12-11 13:36:22 +1100

    finish

--- integration/scvi_integrate.py
+++ integration/scvi_integrate.py
@@ -9,14 +9,33 @@ import scvi
 cellranger_dir = "/directflow/SCCGGroupShare/projects/data/experimental_data/projects/TenK10K/GencodeV44/"
 files = glob.glob(cellranger_dir+"S*")
 
-for file in files:
+# pre-processing steps
+# modify parameters based on sample-specific plots
+def pp(sample_path):
+    adata=sc.read_10x_h5(sample_path+"/outs/filtered_feature_bc_matrix.h5")
+    sc.pp.filter_genes(adata, min_cells = 10)
+#     sc.pp.highly_variable_genes(adata, n_top_genes = 2000, subset = True, flavor = 'seurat_v3')
+#     sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+     
     sample = file.replace(cellranger_dir, "")
-    adata=sc.read_10x_h5(file+"/outs/filtered_feature_bc_matrix.h5")
     adata.var_names_make_unique()
     adata.obs['sample'] = sample
     adata.obs['barcode'] = adata.obs.index
     adata.obs.index = sample + "_" + adata.obs['barcode']
-    datasets.append(adata)
+        
+    sc.pp.filter_cells(adata, min_genes=200) #get rid of cells with fewer than 200 genes
+    #sc.pp.filter_genes(adata, min_cells=3) #get rid of genes that are found in fewer than 3 cells
+    adata.var['mt'] = adata.var_names.str.startswith('mt-')  # annotate the group of mitochondrial genes as 'mt'
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    upper_lim = np.quantile(adata.obs.n_genes_by_counts.values, .98)
+    adata = adata[adata.obs.n_genes_by_counts < upper_lim]
+    adata = adata[adata.obs.pct_counts_mt < 15]
+
+    return adata
+
+datasets = []
+for file in files:
+    datasets.append(pp(file))
     
 adata = datasets[0].concatenate(*datasets[1:])
 sc.tl.pca(adata, svd_solver='arpack')
@@ -31,4 +50,8 @@ sc.pp.neighbors(adata, use_rep = 'X_scVI')
 sc.tl.umap(adata)
 sc.tl.leiden(adata, resolution = 0.5)
 sc.pl.umap(adata, color = ['leiden', 'Sample'], frameon = False)
+adata.uns['scvi_markers'] = markers_scvi
+adata.uns['markers'] = markers
+adata.write_h5ad('integrated.h5ad')
+model.save('model.model')
 
