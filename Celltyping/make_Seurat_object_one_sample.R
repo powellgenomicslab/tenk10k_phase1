3ed33231a200098daa8c96af8017d67aea179e6d
Anna Cuomo <annacuomo92@gmail.com>
2023-12-21 16:06:46 +1100

    single sample to run in parallel

---
+++ Celltyping/make_Seurat_object_one_sample.R
@@ -0,0 +1,25 @@
+args <- commandArgs(trailingOnly=TRUE)
+i <- args[1]
+
+library(Seurat)
+
+# 64 samples from 231013
+cellranger_dir = "/directflow/SCCGGroupShare/projects/data/experimental_data/projects/TenK10K/GencodeV44/"
+cellranger_samples = list.files(cellranger_dir, pattern = "S.") 
+
+out_folder = "/share/ScratchGeneral/anncuo/tenk10k/data_processing/scpred/seurat_objects/"
+
+sample = cellranger_samples[i]
+
+print(paste0("Create Seurat object for ",sample))
+
+cellranger_path = paste0(cellranger_dir,sample,"/outs/filtered_feature_bc_matrix/")
+pbmc.data <- Read10X(data.dir = cellranger_path)
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = sample, min.cells = 3, min.features = 200)
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+out_file = paste0(out_folder, sample, ".rds")
+saveRDS(pbmc, file = out_file)
