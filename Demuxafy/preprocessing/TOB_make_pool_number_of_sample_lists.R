87ccc7dde892684a29c8d4b20c2225bbac493800
Anna Cuomo <annacuomo92@gmail.com>
2024-02-15 15:16:06 +1100

    Update TOB_make_pool_number_of_sample_lists.R

--- Demuxafy/preprocessing/TOB_make_pool_number_of_sample_lists.R
+++ Demuxafy/preprocessing/TOB_make_pool_number_of_sample_lists.R
@@ -13,13 +13,13 @@ samples_dir = "/share/ScratchGeneral/anncuo/tenk10k/data_processing/demuxafy/sam
 ##########################     13 / 10 / 2023     ###########################
 #############################################################################
 
-Samples recap: all TOB, 64 libraries in total:
-
-* Pools sequenced from S0016 to S0037 + 3 maxi pools (S0021-24, S0025-28, S0030-33)
-* All single pools are sequenced three times, e.g., S0018a, S0018b, S0018c
-  * missing here are S0016a, S0017a, S0019a, S0027a, S0037b and S0037c 
-  * S0029 is missing completely (was skipped when pooling)
-* All maxi pools are sequenced twice, e.g., S0021-24a, S0021-24b
+###    Samples recap: all TOB, 64 libraries in total:
+###
+###    * Pools sequenced from S0016 to S0037 + 3 maxi pools (S0021-24, S0025-28, S0030-33)
+###      * All single pools are sequenced three times, e.g., S0018a, S0018b, S0018c
+###      * missing here are S0016a, S0017a, S0019a, S0027a, S0037b and S0037c 
+###      * S0029 is missing completely (was skipped when pooling)
+###    * All maxi pools are sequenced twice, e.g., S0021-24a, S0021-24b
 
 ############################
 
@@ -54,11 +54,11 @@ fwrite(df, output_file, sep="\t")
 ##########################     13 / 12 / 2023     ###########################
 #############################################################################
 
-Samples recap: all TOB, 24 libraries in total:
-
-* Pools sequenced from S0001 to S0008 
-* All single pools are sequenced three times, e.g., S0008a, S0008b, S0008c
-* No maxi pools are included
+###    Samples recap: all TOB, 24 libraries in total:
+###
+###    * Pools sequenced from S0001 to S0008 
+###    * All single pools are sequenced three times, e.g., S0008a, S0008b, S0008c
+###    * No maxi pools are included
 
 ############################
 
@@ -85,25 +85,25 @@ fwrite(df, output_file, sep="\t")
 ##########################     14 / 12 / 2023     ###########################
 #############################################################################
 
-Samples recap: mix of TOB and BioHEART, 41 libraries in total:
-
-TOB (24 libraries):
-
-* Pools sequenced 
-  * S0009 to S0014 
-  * S0038 and S0039
-  * 1 maxi pool (S0034-37)
-* All single pools are sequenced three times, e.g., S0010a, S0010b, S0010c
-  * except S0038 is sequenced four times (S0038a, S0038b, S0038c, S0038d) 
-  * missing here are S0014b, S0014c, S0039c 
-* The maxi pools is sequenced twice, i.e., S0034-37a, S0034-37b
-
-BioHEART (17 libraries and pools):
-
-* Pools sequenced
-  * S0045 to S0053
-  * S0060 to S0067
-* All pools are sequenced just once
+###    Samples recap: mix of TOB and BioHEART, 41 libraries in total:
+###
+###    TOB (24 libraries):
+###
+###    * Pools sequenced 
+###      * S0009 to S0014 
+###      * S0038 and S0039
+###      * 1 maxi pool (S0034-37)
+###    * All single pools are sequenced three times, e.g., S0010a, S0010b, S0010c
+###      * except S0038 is sequenced four times (S0038a, S0038b, S0038c, S0038d) 
+###      * missing here are S0014b, S0014c, S0039c 
+###    * The maxi pools is sequenced twice, i.e., S0034-37a, S0034-37b
+###
+###    BioHEART (17 libraries and pools):
+###
+###    * Pools sequenced
+###      * S0045 to S0053
+###      * S0060 to S0067
+###    * All pools are sequenced just once
 
 ############################
 
@@ -138,23 +138,23 @@ fwrite(df, output_file, sep="\t")
 ##########################     15 / 01 / 2024     ###########################
 #############################################################################
 
-Samples recap: mix of TOB and BioHEART, 25 libraries in total:
-
-TOB (8 libraries):
-
-* Pools sequenced 
-  * entirety of S0015 (S0015a, S0015b, S0015c)
-  * previously missing sequences of S0014 (S0014b, S0014c), S0016 (S0016a) and S0037 (S0037b, S0037c)
-
-BioHEART (17 libraries and pools):
-
-* Pools sequenced
-  * S0040 to S0044
-  * S0054 to S0056 (+ one maxi pool of S0058+S0059 together)
-  * S0135
-  * S0138 and S00139
-  * S0141 to S0145
-* All pools are sequenced just once
+###    Samples recap: mix of TOB and BioHEART, 25 libraries in total:
+###
+###    TOB (8 libraries):
+###
+###    * Pools sequenced 
+###      * entirety of S0015 (S0015a, S0015b, S0015c)
+###      * previously missing sequences of S0014 (S0014b, S0014c), S0016 (S0016a) and S0037 (S0037b, S0037c)
+###
+###    BioHEART (17 libraries and pools):
+###
+###    * Pools sequenced
+###      * S0040 to S0044
+###      * S0054 to S0056 (+ one maxi pool of S0058+S0059 together)
+###      * S0135
+###      * S0138 and S00139
+###      * S0141 to S0145
+###    * All pools are sequenced just once
 
 
 # onto libraries sequenced on Jan 15th, 2024 (240115)
