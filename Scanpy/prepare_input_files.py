d8ac7d164eb294204d1caf5e13b326389895dde0
Anna Cuomo <annacuomo92@gmail.com>
2024-02-19 09:28:21 +1100

    start working on this script

--- Scanpy/prepare_input_files.py
+++ Scanpy/prepare_input_files.py
@@ -1,3 +1,7 @@
+# Specify which directory this will be saved to
+out_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integration/"
+out_file = f'{out_dir}harmony/cpg_test/bespoke_S0040re_42re_maxi44_58and59v2_samples_concatenated/{celltype}_expression_pcs.csv'
+
 # Cell type specific expression
 
 # Cell type specific covariates
