499a3c854f9032b838955659ad3b38ebc793eaac
Anna Cuomo <annacuomo92@gmail.com>
2024-02-19 19:02:17 +1100

    Update and rename prepare_input_files.py to prepare_pheno.py

--- Scanpy/prepare_pheno.py
+++ Scanpy/prepare_pheno.py
@@ -6,12 +6,14 @@ celltype = sys.argv[1]
 chromosome = sys.argv[2]
 
 # Specify which directory the files generated here will be saved to
-out_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/cpg_input_files/harmony_224_samples_concatenated/"
+input_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/"
+input_file = f"{out_dir}224_libraries/concatenated_gene_info.csv"
 
 # Specify which files this script will generate
+output_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/cpg_anndata/"
 
 # AnnData object: expression + gene info + batch info (batch=sequencing library)
-adata_out_file = f'{out_dir}/{celltype}_chr{chromosome}.h5ad'
+adata_out_file = f'{output_dir}/{celltype}_chr{chromosome}.h5ad'
 
 # CSV for expression covariates (principal components after Harmony correction)
 pcs_out_file = f'{out_dir}/{celltype}_expression_pcs.csv'
@@ -19,31 +21,14 @@ pcs_out_file = f'{out_dir}/{celltype}_expression_pcs.csv'
 ##############################################################################################
 
 # Load integrated AnnData object
-adata = sc.read(file)
-
-# Load gencode (v44) gene info file
-gencode_text_file = '/share/ScratchGeneral/anncuo/reference_data/gencode.v44.basic.annotation_df.txt'
-gene_info = pd.read_csv(gencode_text_file)
-
-# Add gene info to AnnData
-# set gene ids as indices
-adata.var.index = [gene for gene in adata.var['gene_ids']]
-genes_cellranger = adata.var.index
-
-# Remove gene version
-gene_info.index = [gene.split(".")[0] for gene in gene_info['gene_id']]
-
-# Only consider genes in AnnData object
-gene_info_df = gene_info[gene_info.index.isin(genes_cellranger)]
-
-# Add info
-adata.var = pd.concat([adata.var,gene_info_df], axis=1)
+adata = sc.read(input_file)
 
 # Extract cell type and chromosome specific expression
-adata_CD4_Naive = adata[adata.obs['wg2_scpred_prediction'] == celltype]
-adata_chr1 = adata_CD4_Naive[:, adata_CD4_Naive.var["chr"] == f'chr{chromosome}']
-adata_chr1.obs['cell'] = adata_chr1.obs.index
-adata_chr1.write(adata_out_file)
+adata_ct = adata[adata.obs['wg2_scpred_prediction'] == celltype]
+adata_ct_chr = adata_ct[:, adata_ct.var["chr"] == f'chr{chromosome}']
+adata_ct_chr.obs['cell'] = adata_ct_chr.obs.index
+adata_ct_chr.write(adata_out_file)
+
 
 # normalisation
 sc.pp.normalize_total(adata, target_sum=1e4)
