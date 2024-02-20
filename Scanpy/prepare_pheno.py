665efa1ddae96027261033954bf4edb0d792075f
Anna Cuomo <annacuomo92@gmail.com>
2024-02-20 11:29:44 +1100

    rearrange and fix typo

--- Scanpy/prepare_pheno.py
+++ Scanpy/prepare_pheno.py
@@ -5,17 +5,15 @@ import scanpy as sc
 celltype = sys.argv[1]
 chromosome = sys.argv[2]
 
-# Specify which directory the files generated here will be saved to
-input_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/"
-input_file = f"{out_dir}224_libraries/concatenated_gene_info.csv"
-
 # Specify which files this script will generate
 output_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/cpg_anndata/"
-
+# Specify which directory the files generated here will be saved to
 # AnnData object: expression + gene info + batch info (batch=sequencing library)
 adata_out_file = f'{output_dir}/{celltype}_chr{chromosome}.h5ad'
 
 # Load integrated AnnData object
+input_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/"
+input_file = f"{input_dir}224_libraries/concatenated_gene_info.csv"
 adata = sc.read(input_file)
 
 # Extract cell type and chromosome specific expression
