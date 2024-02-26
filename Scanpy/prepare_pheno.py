2277ba80f35138b19065c85ef0c71c1b31de773d
Anna Cuomo <annacuomo92@gmail.com>
2024-02-26 14:11:55 +1100

    add print statements

--- Scanpy/prepare_pheno.py
+++ Scanpy/prepare_pheno.py
@@ -5,12 +5,17 @@ import scanpy as sc
 celltype = sys.argv[1]
 chromosome = sys.argv[2]
 
+print(celltype)
+print(chromosome)
+
 # Specify which files this script will generate
 output_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/cpg_anndata"
 # Specify which directory the files generated here will be saved to
 # AnnData object: expression + gene info + batch info (batch=sequencing library)
 ct = celltype.replace(" ","_") # remove spaces from cell type names
 adata_out_file = f'{output_dir}/{ct}_chr{chromosome}.h5ad'
+
+print(adata_out_file)
 
 # Load integrated AnnData object
 input_dir = "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/integrated_objects/"
