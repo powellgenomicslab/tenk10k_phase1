c76f2b70eead0aea9920f8e6d91c9856702712e5
blabow <blabow@brenner-fpga.garvan.unsw.edu.au>
2024-06-28 09:28:43 +1000

    update plot to examine number of samples with over 5,10,20,50 cells

--- data_visualisation/cell_type_threshold_analysis.R
+++ data_visualisation/cell_type_threshold_analysis.R
@@ -3,6 +3,7 @@ library(glue)
 library(ggsci)
 library(RColorBrewer)
 library(patchwork)
+library(tidyverse)
 
 # read in the cell-level metadata
 cell_metadata <- read_csv("/directflow/SCCGGroupShare/projects/blabow/tenk10k_phase1/data_processing/scanpy/output/integrated_objects/240_libraries_archive/240_libraries_celltypes_by_cohort_indiv.csv") %>%
@@ -13,13 +14,17 @@ cell_metadata %>%
 
 plot_data <- cell_metadata %>%
     group_by(wg2_scpred_prediction, individual) %>%
-    # calculate which samples have > 3 cells
+    # calculate which samples have cells above each threshold
     mutate(
         three_or_more_cells = n() >= 3,
+        five_or_more_cells = n() >= 5,
+        ten_or_more_cells = n() >= 10,
+        twenty_or_more_cells = n() >= 20,
+        fifty_or_more_cells = n() >= 50,
         total_cells = n()
     ) %>%
     ungroup() %>%
-    select(individual, wg2_scpred_prediction, three_or_more_cells, total_cells) %>%
+    select(individual, wg2_scpred_prediction, three_or_more_cells, five_or_more_cells, ten_or_more_cells, twenty_or_more_cells, fifty_or_more_cells, total_cells) %>%
     distinct() %>%
     arrange(total_cells) %>%
     filter(three_or_more_cells == TRUE)
@@ -82,5 +87,60 @@ combined %>%
         filename = "/directflow/SCCGGroupShare/projects/blabow/tenk10k_phase1/data_processing/scanpy/output/integrated_objects/figures/ggBarplot_over_3_cells_combined_plots.png",
         width = 10,
         height = 8,
+        dpi = 300
+    )
+
+cell_threshold_data <- cell_metadata %>%
+    group_by(wg2_scpred_prediction, individual) %>%
+    # calculate which samples have cells above each threshold
+    mutate(
+        three_or_more_cells = n() >= 3,
+        five_or_more_cells = n() >= 5,
+        ten_or_more_cells = n() >= 10,
+        twenty_or_more_cells = n() >= 20,
+        fifty_or_more_cells = n() >= 50,
+        total_cells = n()
+    ) %>%
+    ungroup() %>%
+    select(individual, wg2_scpred_prediction, three_or_more_cells, five_or_more_cells, ten_or_more_cells, twenty_or_more_cells, fifty_or_more_cells) %>%
+    distinct() %>%
+    group_by(wg2_scpred_prediction) %>%
+    summarise(
+        three_or_more_cells = sum(three_or_more_cells),
+        five_or_more_cells = sum(five_or_more_cells),
+        ten_or_more_cells = sum(ten_or_more_cells),
+        twenty_or_more_cells = sum(twenty_or_more_cells),
+        fifty_or_more_cells = sum(fifty_or_more_cells)
+    ) %>%
+    ungroup() %>%
+    pivot_longer(!wg2_scpred_prediction, names_to = "cell_number_threshold", values_to = "total_individuals") %>%
+    mutate(cell_number_threshold = factor(cell_number_threshold, levels = c(
+        "three_or_more_cells",
+        "five_or_more_cells",
+        "ten_or_more_cells",
+        "twenty_or_more_cells",
+        "fifty_or_more_cells"
+    )))
+
+bar_plot_passing_thresholds_multi <- cell_threshold_data %>%
+    ggplot(aes(x = reorder(wg2_scpred_prediction, -total_individuals), y = total_individuals)) +
+    facet_wrap(~cell_number_threshold, ncol = 1) +
+    geom_bar(fill = "skyblue", stat = "identity") +
+    theme_bw() +
+    geom_hline(yintercept = 100, linetype = "dashed", colour = "red") +
+    xlab("Cell type") +
+    ylab("Number of individuals") +
+    theme(
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        axis.title.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()
+    )
+
+bar_plot_passing_thresholds_multi %>%
+    ggsave(
+        filename = "/directflow/SCCGGroupShare/projects/blabow/tenk10k_phase1/data_processing/scanpy/output/integrated_objects/figures/ggBarplot_cell_types_passing_thresholds_multi_thresholds.png",
+        width = 14,
+        height = 16,
         dpi = 300
     )
