9fa18a97543653e3ae44adf41dbc99391eadbd4e
blabow <blabow@brenner-fpga.garvan.unsw.edu.au>
2024-07-15 18:11:52 +1000

    added new script that's able to generate the pool - donor mappings for both TOB and bioheart. Updated the master data creation script to enable this

--- Demuxafy/preprocessing/create_master_metadata.R
+++ Demuxafy/preprocessing/create_master_metadata.R
@@ -2,65 +2,76 @@
 library(tidyverse)
 library(janitor)
 library(RSQLite)
+library(glue)
 
 # read cpg mappings and donor pools info
 cpg_map_file <- "/directflow/SCCGGroupShare/projects/anncuo/TenK10K_pilot/tenk10k/data_processing/str_sample-sex-mapping_sample_karyotype_sex_mapping.csv"
-donor_pools_bioheart_file <- "/share/ScratchGeneral/anncuo/tenk10k/donor_info/TenK10K_BioHeart_pool_info_v3.csv"
+donor_pools_bioheart_file <- "/directflow/SCCGGroupShare/projects/blabow/tenk10k_phase1/data_processing/TenK10K_BioHeart_pool_info_v5.csv"
 donor_pools_tob_file <- "/share/ScratchGeneral/anncuo/tenk10k/donor_info/TOB_TenK10K_pool_info_v3.csv"
 onek1k_file_name <- "/directflow/SCCGGroupShare/projects/data/experimental_data/projects/OneK1K/published_data/Database/OneK1K_MetadataDB.db"
 
 # CPG ID's (from WGS)
 cpg_mapping <- read_csv(cpg_map_file) %>%
-    rename(cpg_id = s)
+    rename(cpg_id = s) %>%
+    select(-sex_karyotype) %>%
+    mutate(notes = if_else(str_detect(external_id, "-PBMC"), glue("Alternate external ID: {external_id}. -PBMC suffix indicates that these samples had WGS re-run due to low quality."), NA)) %>%
+    mutate(external_id = str_remove(external_id, "-PBMC"))
 
 # Bioheart
 donor_pools_bioheart <- read_csv(donor_pools_bioheart_file) %>%
     janitor::clean_names() %>%
     mutate(bioheart_cohort = TRUE) %>%
     rename(
-        bioheart_id = sample_id,
+        bioheart_id = bio_heart_id,
         bioheart_pool = pool
-    )
+    ) %>%
+    select(-cpg_id)
 
 # TOB (from onek1k)
 donor_pools_tob <- read_csv(donor_pools_tob_file) %>%
     janitor::clean_names() %>%
-    mutate(external_id_modified = tob_id %>% str_remove("-0")) %>% # clean tob mapping id so the data can be joined
+    # mutate(external_id_modified = tob_id %>% str_remove("-0")) %>% # clean tob mapping id so the data can be joined
     mutate(tob_cohort = TRUE) %>%
     mutate(onek1k_cohort = TRUE)
 
 # Onek1k metadata
 sqlite.driver <- dbDriver("SQLite")
 db <- dbConnect(sqlite.driver, dbname = onek1k_file_name)
-onek1k_metadata <- dbReadTable(db, "RECRUITED_DONORS") %>%
+tob_id_mapping <- dbReadTable(db, "RECRUITED_DONORS") %>%
     tibble() %>%
-    janitor::clean_names()
+    janitor::clean_names() %>%
+    select(patient_id, tob_id) %>%
+    distinct()
 
-nrow(onek1k_metadata)
-nrow(donor_pools_tob)
-colnames(donor_pools_tob)
+# join the TOB/onek1k data
+donor_pools_tob <- donor_pools_tob %>%
+    left_join(tob_id_mapping, by = "tob_id")
 
-# join the TOB data
-all_tob <- onek1k_metadata %>%
-    full_join(donor_pools_tob, by = "tob_id")
-
-table(duplicated(donor_pools_tob$tob_id))
+# NOTES:
+# 1. Full join BH pool info with TOB/CPG mappings: includes BH samples even if they don't currently have CPG-ids
+# 2: there is one duplicated TOB sample that is not marked as a repeat, although it maps to 2 different pools so probably is just an un-marked repeat sample
+# 3. keeps the notes in there as they are useful for keeping track of the repeated samples
 
 master_table <- cpg_mapping %>%
-    mutate(external_id_modified = external_id %>% str_remove("-PBMC")) %>%
-    full_join(donor_pools_tob, by = "external_id_modified") %>%
-    full_join(donor_pools_bioheart, by = c("external_id_modified" = "bioheart_id")) %>%
-    mutate(tenk10k_pool = coalesce(tenk10k_pool, bioheart_pool))
-
-nrow(donor_pools_tob)
-sum(!is.na(master_table$tob_id))
+    left_join(donor_pools_tob, by = c("external_id" = "patient_id")) %>%
+    mutate(notes = coalesce(notes.x, notes.y)) %>%
+    select(-notes.x, -notes.y) %>%
+    # full join so that samples without CPG_ids are included (while waiting for WGS)
+    full_join(donor_pools_bioheart, by = c("external_id" = "bioheart_id")) %>%
+    mutate(notes = coalesce(notes.x, notes.y)) %>%
+    select(-notes.x, -notes.y) %>%
+    mutate(tenk10k_pool = coalesce(tenk10k_pool, bioheart_pool)) %>%
+    select(cpg_id, external_id, tob_id, tenk10k_pool, tob_cohort, bioheart_cohort, notes)
 
 master_table %>%
     write_csv("data_processing/tenk10k_donor_pool_info.csv")
 
-# TODO: add which cohort the donor is from (TOB, bioheart, onek1k)
-# TODO: add which pool each cohort is from
-# TODO: add sequencing batches - each row will correspond to the sequencing batch i.e. will include the repeats
-cpg_mapping %>% write_csv("data_processing/str_sample-sex-mapping_sample_karyotype_sex_mapping.csv")
-
-onek1k_metadata %>% write_csv("data_processing/OneK1K_Metadata_donors.csv")
+# check ID duplication:
+# duplicated_ids <- master_table %>%
+#     filter(duplicated(external_id)) %>%
+#     select(external_id) %>%
+#     distinct() %>%
+#     pull(external_id)
+# master_table %>%
+#     filter(external_id %in% duplicated_ids) %>%
+#     print(n = 100)
