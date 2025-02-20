fbccfcf8c01441b971d7dfe3b809f52d5fd9f2f6
blake-bowen <blakebowen97@gmail.com>
2025-02-20 15:05:36 +1100

    update color palette for major cell types

--- plotting_notebooks/overview_figures/manuscript_figures/tenk_data_vis_utils.R
+++ plotting_notebooks/overview_figures/manuscript_figures/tenk_data_vis_utils.R
@@ -101,46 +101,46 @@ grey <- pal_material("grey")(10)
 # NOTE: the "color" column is used in downstream plotting, modifying this will update the colour scheme for all downstream plots
 
 tenk_color_pal <- tribble(
-    ~wg2_scpred_prediction, ~color_material, ~color_biorender1, ~color_biorender2, ~color,
+    ~wg2_scpred_prediction, ~color_material, ~color_biorender1, ~color_biorender2, ~color, ~color_major_cell_type,
     # Lymphoid
     # CD4 T cells
-    "CD4_TCM", blue[10], "#AB728A", "#9C85C3", deeppurple[10],
-    "CD4_Naive", blue[8], "#83576A", "#605278", deeppurple[8],
-    "CD4_TEM", blue[6], "#C4839E", "#8773A8", deeppurple[6],
-    "CD4_CTL", blue[4], "#DA91B0", "#B197DD", deeppurple[4],
-    "Treg", blue[3], "#F4BFE0", "#DFB4EC", deeppurple[3],
-    "CD4_Proliferating", blue[2], "#DA91B0", "#D4ACE1", deeppurple[2],
+    "CD4_TCM", blue[10], "#AB728A", "#9C85C3", deeppurple[10], deeppurple[5],
+    "CD4_Naive", blue[8], "#83576A", "#605278", deeppurple[8], deeppurple[5],
+    "CD4_TEM", blue[6], "#C4839E", "#8773A8", deeppurple[6], deeppurple[5],
+    "CD4_CTL", blue[4], "#DA91B0", "#B197DD", deeppurple[4], deeppurple[5],
+    "Treg", blue[3], "#F4BFE0", "#DFB4EC", deeppurple[3], deeppurple[5],
+    "CD4_Proliferating", blue[2], "#DA91B0", "#D4ACE1", deeppurple[2], deeppurple[5],
     # Unconventional T
-    "gdT", lime[10], "#A7B4D3", "#DE94B4", pink[10],
-    "MAIT", lime[8], "#5D6E8D", "#B0758E", pink[8],
-    "dnT", lime[6], "#6A81B5", "#EE9FC1", pink[6],
-    "ILC", lime[4], "#CDDAE7", "#BB7D97", pink[4],
+    "gdT", lime[10], "#A7B4D3", "#DE94B4", pink[10], pink[5],
+    "MAIT", lime[8], "#5D6E8D", "#B0758E", pink[8], pink[5],
+    "dnT", lime[6], "#6A81B5", "#EE9FC1", pink[6], pink[5],
+    "ILC", lime[4], "#CDDAE7", "#BB7D97", pink[4], pink[5],
     # CD8 T cells
-    "CD8_TEM", deeppurple[10], "#749C93", "#6A81B5", blue[10],
-    "CD8_Naive", deeppurple[7], "#57736E", "#5D6E8D", blue[7],
-    "CD8_TCM", deeppurple[5], "#70B1A4", "#A7B4D3", blue[5],
-    "CD8_Proliferating", deeppurple[2], "#8BD1BF", "#CDDAE7", blue[2],
+    "CD8_TEM", deeppurple[10], "#749C93", "#6A81B5", blue[10], blue[5],
+    "CD8_Naive", deeppurple[7], "#57736E", "#5D6E8D", blue[7], blue[5],
+    "CD8_TCM", deeppurple[5], "#70B1A4", "#A7B4D3", blue[5], blue[5],
+    "CD8_Proliferating", deeppurple[2], "#8BD1BF", "#CDDAE7", blue[2], blue[5],
     ## NK cells
-    "NK", pink[9], "#708A5F", "#708A5F", green[9],
-    "NK_CD56bright", pink[6], "#96B980", "#96B981", green[6],
-    "NK_Proliferating", pink[3], "#BAE49E", "#BAE49E", green[3],
+    "NK", pink[9], "#708A5F", "#708A5F", green[9], green[5],
+    "NK_CD56bright", pink[6], "#96B980", "#96B981", green[6], green[5],
+    "NK_Proliferating", pink[3], "#BAE49E", "#BAE49E", green[3], green[5],
     ## B cells
-    "B_naive", yellow[10], "#EEE0B6", "#EEE0B7", yellow[10],
-    "B_intermediate", yellow[8], "#F8ED7D", "#F8ED7D", yellow[8],
-    "B_memory", yellow[6], "#E2D872", "#E2D872", yellow[6],
-    "Plasmablast", yellow[4], "#B5AE5C", "#B5AE5C", yellow[4],
+    "B_naive", yellow[10], "#EEE0B6", "#EEE0B7", yellow[10], yellow[5],
+    "B_intermediate", yellow[8], "#F8ED7D", "#F8ED7D", yellow[8], yellow[5],
+    "B_memory", yellow[6], "#E2D872", "#E2D872", yellow[6], yellow[5],
+    "Plasmablast", yellow[4], "#B5AE5C", "#B5AE5C", yellow[4], yellow[5],
 
     ## Monocyte
     # myeloid
-    "CD14_Mono", green[8], "#F3AF84", "#F3AF84", deeporange[9],
-    "CD16_Mono", green[4], "#DD9F78", "#DD9F78", deeporange[3],
+    "CD14_Mono", green[8], "#F3AF84", "#F3AF84", deeporange[9], deeporange[5],
+    "CD16_Mono", green[4], "#DD9F78", "#DD9F78", deeporange[3], deeporange[5],
     ## DC
-    "cDC2", brown[8], "#9C4C49", "#9C4C49", brown[8],
-    "pDC", brown[6], "#ED7470", "#ED7470", brown[6],
-    "cDC1", brown[4], "#6E3634", "#6E3634", brown[4],
-    "ASDC", brown[2], "#CF6662", "#CF6662", brown[2],
+    "cDC2", brown[8], "#9C4C49", "#9C4C49", brown[8], brown[5],
+    "pDC", brown[6], "#ED7470", "#ED7470", brown[6], brown[5],
+    "cDC1", brown[4], "#6E3634", "#6E3634", brown[4], brown[5],
+    "ASDC", brown[2], "#CF6662", "#CF6662", brown[2], brown[5],
     # Other
-    "HSPC", grey[5], "#A9A9A9", "#A9A9A9", grey[5],
+    "HSPC", grey[5], "#A9A9A9", "#A9A9A9", grey[5], grey[5]
     #
     # "Platelet", brown[1],
     # "Eryth", brown[2],
