3e12098a366716f1c4ca467b4885354025f11460
Anna Cuomo <annacuomo92@gmail.com>
2025-05-30 21:18:46 +1200

    Update get_celltype_mean_expression.py

--- Scanpy/get_celltype_mean_expression.py
+++ Scanpy/get_celltype_mean_expression.py
@@ -22,5 +22,6 @@ for celltype in celltypes:
     # save as pandas data frame
     expr_df = pd.DataFrame(data=means_by_ct.layers['mean'], index=[f'mean_{celltype}'], columns = means_by_ct.var.index)
     # write to file
+    print('Writing to file: {celltype}, {chrom}')
     out_file = f'{out_dir}{celltype}_chr{chrom}.csv'
     expr_df.to_csv(out_file)
